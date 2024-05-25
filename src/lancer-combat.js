345f8840b07b7fba23279175f0b7821a6f555478
Joe <182554+BoltsJ@users.noreply.github.com>
2024-05-25 18:40:07 -0500

    Fix v12 deprecation warning
    This also removes v11 compatibility

--- src/lancer-combat.js
+++ src/lancer-combat.js
@@ -207,7 +207,7 @@ export class LancerCombatant extends Combatant {
     if (num === 0) return this;
     return this.update({
       [`flags.${module}.activations`]: {
-        value: Math.clamped((this.activations?.value ?? 0) + num, 0, this.activations?.max ?? 1),
+        value: Math.clamp((this.activations?.value ?? 0) + num, 0, this.activations?.max ?? 1),
       },
     });
   }
